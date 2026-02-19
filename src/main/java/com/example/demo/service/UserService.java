6574a9ef98e00ded7cea93ea63955b1c8da6e654
Prajwal Badiger <150665648+Prajwalbadiger76@users.noreply.github.com>
2026-02-19 12:45:49 +0530

    Implemented authentication module with JWT, login and register APIs (#3)

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.RegisterRequest;
+
+public interface UserService {
+
+    void register(RegisterRequest request);
+
+    String login(LoginRequest request);
+}
