c0b49cedefcd539674193f2523e94cbe348ccb5a
BhavyaB22 <bhavyabasam22@gmail.com>
2026-02-23 11:54:05 +0530

    Updated Post entity, repository

--- src/main/java/com/example/demo/repo/PostRepository.java
+++ src/main/java/com/example/demo/repo/PostRepository.java
@@ -1,5 +1,14 @@
 package com.example.demo.repo;
 
-public class PostRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.Post;
+
+@Repository
+// CHANGE 'class' TO 'interface'
+public interface PostRepository extends JpaRepository<Post, Long> {
+    
+    List<Post> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
--- src/main/java/com/example/demo/service/PostServiceImpl.java
+++ src/main/java/com/example/demo/service/PostServiceImpl.java
@@ -1,11 +1,12 @@
 package com.example.demo.service;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.dto.PostRequestDTO;
-import com.example.demo.entity.Posts;
-import com.example.demo.repository.PostRepository;
+import com.example.demo.entity.*;
+import com.example.demo.repo.PostRepository;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.exception.UnauthorizedActionException;
 
@@ -16,8 +17,8 @@ public class PostServiceImpl implements PostService {
 	private PostRepository postRepository;
 	
 	@Override
-	public Posts savePost(PostRequestDTO dto, Long userId) {
-		Posts post = new Posts();
+	public Post savePost(PostRequestDTO dto, Long userId) {
+		Post post = new Post();
 		post.setContent(dto.getContent());
 		post.setHashtags(dto.getHashtags());
 		post.setUserId(userId);
@@ -29,7 +30,7 @@ public class PostServiceImpl implements PostService {
 
 	@Override
 	public void deletePost(Long postId, Long userId) {
-		Posts post = postRepository.findById(postId)
+		Post post = postRepository.findById(postId)
 				.orElseThrow(() -> new ResourceNotFoundException("Post not found"));
 		
 		if (!post.getUserId().equals(userId)) {
@@ -39,7 +40,7 @@ public class PostServiceImpl implements PostService {
 	}
 
 	@Override
-	public List<Posts> getUserFeed(Long userId) {
+	public List<Post> getUserFeed(Long userId) {
 		return postRepository.findByUserIdOrderByCreatedAtDesc(userId);
 	}
 }
