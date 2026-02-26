4e1c5cc6051e475ffde4bac1a0e460cf21c102f8
Prajwal Badiger <prajwalbadiger7676@gmail.com>
2026-02-27 00:42:38 +0530

    test: add unit tests for AuthController and UserServiceImpl

--- src/test/java/com/example/demo/RevConnectCapstoneApplicationTests.java
+++ src/test/java/com/example/demo/RevConnectCapstoneApplicationTests.java
@@ -3,11 +3,10 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RevConnectCapstoneApplication.class)
 class RevConnectCapstoneApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
